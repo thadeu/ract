1242dfefdd2180ae6d5ca87ccaadc8e593cbe04e
thadeu <tadeuu@gmail.com>
2025-04-18 01:43:00 -0300

    v0.3.0

--- lib/ract/version.rb
+++ lib/ract/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true # :rdoc
 
 class Ract
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 end
--- spec/lib/ract/version_spec.rb
+++ spec/lib/ract/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe Ract::VERSION do
   it 'has a version number' do
-    expect(Ract::VERSION).to eq('0.2.0')
+    expect(Ract::VERSION).to eq('0.3.0')
   end
 end
